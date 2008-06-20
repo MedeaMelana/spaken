c2ab14d47db9c1ca8dacc6151ece3b75ca18df3d
MedeaMelana <MedeaMelana@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-20 20:55:56 +0000

    Eerste klassetjes.

---
+++ trunk/src/spaken/model/Point.java
@@ -0,0 +1,17 @@
+/* Created on Jun 20, 2008. */
+package spaken.model;
+
+public class Point {
+
+	private double x;
+	private double y;
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+}
