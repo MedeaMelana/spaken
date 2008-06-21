c02969984aaa0da4d9e5c535d5090d0878a9e4ab
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-21 15:52:13 +0000

    Een leeg venster! Woehoe!

---
+++ trunk/src/spaken/ui/swt/Main.java
@@ -0,0 +1,24 @@
+package spaken.ui.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+public class Main {
+
+  public static void main(final String[] args) {
+    Display display = new Display();
+
+    final Shell shell = new Shell(display);
+    shell.setLayout(new FillLayout());
+    Composite composite = new Composite(shell, SWT.EMBEDDED);
+    
+    shell.open();
+    while(!shell.isDisposed()) {
+	    if (!display.readAndDispatch()) display.sleep();
+    }
+
+    display.dispose ();
+  }
+
+} 
