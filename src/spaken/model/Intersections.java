57ead5990bb171ef467efdbf909cb452070efccc
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-22 20:54:41 +0000

    Implementatie line-line-intersectie. Stukje probeercode ervoor aan Main gehangen.

--- src/spaken/model/Intersections.java
+++ src/spaken/model/Intersections.java
@@ -2,15 +2,55 @@ package spaken.model;
 
 public class Intersections {
   
-  public Point intersect(Line l1, Line l2) {
-    return null;
+  public static Point intersect(final Line l1, final Line l2) {
+    return new Point() {
+      public double getCoor(boolean giveX) {
+        Point p1 = l1.getP1();
+        Point p2 = l1.getP2();
+        Point p3 = l2.getP1();
+        Point p4 = l2.getP2();
+        
+        double p1x = p1.getX();
+        double p2x = p2.getX();
+        double p3x = p3.getX();
+        double p4x = p4.getX();
+        double p1y = p1.getY();
+        double p2y = p2.getY();
+        double p3y = p3.getY();
+        double p4y = p4.getY();
+        
+        // formule van http://en.wikipedia.org/wiki/Line-line_intersection
+        
+        double a, b;
+        
+        if (giveX) {
+          a = p3x - p4x;
+          b = p1x - p2x;
+        } else {
+          a = p3y - p4y;
+          b = p1y - p2y;
+        }
+        
+        return ((p1x*p2y - p1y*p2x) * a - b * (p3x*p4y - p3y*p4x))
+             / ((p1x - p2x) * (p3y - p4y) - (p1y - p2y) * (p3x - p4x));
+      }
+      
+    
+      public double getX() {
+        return getCoor(true);
+      }
+      
+      public double getY() {
+        return getCoor(false);
+      }
+    };
   }
   
-  public Point[] intersect(Circle c1, Circle c2) {
+  public static Point[] intersect(Circle c1, Circle c2) {
     return new Point[2];
   }
 	
-  public Point[] intersect(Line l, Circle c) {
+  public static Point[] intersect(Line l, Circle c) {
     return new Point[2];
   }
   
--- src/spaken/ui/swt/Main.java
+++ src/spaken/ui/swt/Main.java
@@ -1,5 +1,7 @@
 package spaken.ui.swt;
 
+import spaken.model.*;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
@@ -7,6 +9,8 @@ import org.eclipse.swt.widgets.*;
 public class Main {
 
   public static void main(final String[] args) {
+    quickTest();
+    
     Display display = new Display();
 
     final Shell shell = new Shell(display);
@@ -33,6 +37,20 @@ public class Main {
     
     item = new ToolItem(tools, SWT.RADIO);
     item.setText("Potlood");
+  }
+  
+  // Do a little testing
+  public static void quickTest() {
+    Point p1 = new FixedPoint(0,1);
+    Point p2 = new FixedPoint(3,4);
+    Point p3 = new FixedPoint(-1,3);
+    Point p4 = new FixedPoint(3,1);
+    
+    Line l1 = new Line(p1,p2);
+    Line l2 = new Line(p3,p4);
+    
+    Point i = Intersections.intersect(l1, l2);
+    System.out.println("x=" + i.getX() + ", y=" + i.getY());
   }
 
 } 
