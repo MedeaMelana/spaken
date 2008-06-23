fd3ca15a7905996a4d65d76ba6986b011f9f1487
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-23 19:11:42 +0000

    Zorg dat Points locatieloos kunnen zijn.

---
+++ src/spaken/model/ImaginaryPointException.java
@@ -0,0 +1,21 @@
+package spaken.model;
+
+public class ImaginaryPointException extends Exception {
+  private static final String MESSAGE = "Attempt to use imaginary Point";
+  private final Point point;
+  
+  public ImaginaryPointException(Point p) {
+    super(MESSAGE);
+    this.point = p;
+  }
+  
+  public ImaginaryPointException() {
+    super(MESSAGE);
+    this.point = null;
+  }
+
+  public Point getPoint() {
+    return point;
+  }
+  
+}
