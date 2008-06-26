b291b0d397b2e458e2acc2582378738818787b7c
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-26 21:44:59 +0000

    Preview ook points; previewkleur wat lichter.

--- src/spaken/ui/swing/PointCreateTool.java
+++ src/spaken/ui/swing/PointCreateTool.java
@@ -1,10 +1,14 @@
 package spaken.ui.swing;
 
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 
-import spaken.model.FixedPoint;
+import spaken.model.*;
+import spaken.model.rendered.RenderedPoint;
 
 public class PointCreateTool extends AbstractTool {
+
+	private Pos mouse;
 
 	protected PointCreateTool() {
 		super("Create Point");
@@ -14,6 +18,25 @@ public class PointCreateTool extends AbstractTool {
 		FixedPoint pt = new FixedPoint(e.getX(), e.getY());
 		canvas.getSpace().add(pt);
 		canvas.refresh();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		canvas.refresh();
+		mouse = new Pos(e.getX(), e.getY());
+	}
+
+	@Override
+	public void drawState(Graphics2D g, double pixelSize) {
+		if (mouse != null) {
+			new RenderedPoint(mouse, true, DrawingConstants.OUTLINE).draw(g, pixelSize);
+		}
+	}
+
+	@Override
+	public void uninstall(SpaceCanvas canvas) {
+		super.uninstall(canvas);
+		mouse = null;
 	}
 
 }
