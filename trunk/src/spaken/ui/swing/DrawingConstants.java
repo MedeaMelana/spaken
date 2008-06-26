5090481a33e5fc383dfba1ec887b2b38561a3233
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-26 21:44:59 +0000

    Preview ook points; previewkleur wat lichter.

--- trunk/src/spaken/ui/swing/DrawingConstants.java
+++ trunk/src/spaken/ui/swing/DrawingConstants.java
@@ -6,7 +6,7 @@ import static java.awt.Color.*;
 public class DrawingConstants {
 	public static final Color BACKGROUND = WHITE;
 	public static final Color FOREGROUND = BLACK;
-	public static final Color OUTLINE = GRAY;
+	public static final Color OUTLINE = new Color(0xdddddd);
 	public static final Color HIGHLIGHT = new Color(0xff7f7f);
 	public static final Color CONTROLLABLE = new Color(0x7f7fff);
 
