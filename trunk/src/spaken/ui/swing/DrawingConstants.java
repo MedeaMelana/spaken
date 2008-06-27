3428e26640bb56efdf9606256f5f3765422a5987
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-27 12:56:29 +0000

    Uitgesprokener kleurtjes.

--- trunk/src/spaken/ui/swing/DrawingConstants.java
+++ trunk/src/spaken/ui/swing/DrawingConstants.java
@@ -6,9 +6,9 @@ import static java.awt.Color.*;
 public class DrawingConstants {
 	public static final Color BACKGROUND = WHITE;
 	public static final Color FOREGROUND = BLACK;
-	public static final Color OUTLINE = new Color(0xdddddd);
-	public static final Color HIGHLIGHT = new Color(0xff7f7f);
-	public static final Color CONTROLLABLE = new Color(0x7f7fff);
+	public static final Color OUTLINE = new Color(0xb0b0b0);
+	public static final Color HIGHLIGHT = new Color(0xff4040);
+	public static final Color CONTROLLABLE = new Color(0x4040ff);
 
 	public static final double POINT_SIZE = 5;
 	public static final double POINT_SELECT_SIZE = 10;
