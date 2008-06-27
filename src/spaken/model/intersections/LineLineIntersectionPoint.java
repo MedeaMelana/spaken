78616677ab09824b0c72004e96b4a488bb1154b7
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-06-27 15:28:03 +0000

    Refactor intersections tot nette klassen.

---
+++ src/spaken/model/intersections/LineLineIntersectionPoint.java
@@ -0,0 +1,41 @@
+package spaken.model.intersections;
+
+import spaken.model.*;
+
+public class LineLineIntersectionPoint extends DerivedPoint {
+	
+	private Line l1, l2;
+	
+	LineLineIntersectionPoint(Line l1, Line l2) {
+		this.l1 = l1;
+		this.l2 = l2;
+	}
+
+	public Pos getPos() throws ImaginaryPointException {
+		Pos p1 = l1.getP1().getPos();
+		Pos p2 = l1.getP2().getPos();
+		Pos p3 = l2.getP1().getPos();
+		Pos p4 = l2.getP2().getPos();
+
+		// formule van
+		// http://en.wikipedia.org/w/index.php?title=Line-line_intersection&oldid=210305729
+
+		double w = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y)
+				* (p3.x - p4.x);
+
+		if (w == 0) {
+			throw new ImaginaryPointException();
+		}
+
+		double u = p1.x * p2.y - p1.y * p2.x;
+		double v = p3.x * p4.y - p3.y * p4.x;
+
+		double ax = p3.x - p4.x;
+		double bx = p1.x - p2.x;
+		double ay = p3.y - p4.y;
+		double by = p1.y - p2.y;
+
+		return new Pos((u * ax - bx * v) / w, (u * ay - by * v) / w);
+	}
+
+}
