5b9e38b9ed4f10c670be6235877184172b0dd415
MedeaMelana <MedeaMelana@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-07-01 13:12:48 +0000

    Fixed transformations for PointCreateTool.

--- src/spaken/model/Pos.java
+++ src/spaken/model/Pos.java
@@ -1,16 +1,28 @@
 package spaken.model;
 
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Point2D;
+
 /**
  * An object of the <tt>Pos</tt> class represents a fixed position in space.
  */
 public class Pos {
-  public static final Pos ZERO = new Pos(0,0);
-  
+	public static final Pos ZERO = new Pos(0, 0);
+
 	public final double x, y;
 
 	public Pos(double x, double y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public Pos(Point2D p) {
+		this(p.getX(), p.getY());
+	}
+
+	public Point2D toPoint2D() {
+		return new Point2D.Double(x, y);
 	}
 
 	public double getX() {
@@ -72,22 +84,38 @@ public class Pos {
 	public double size() {
 		return Math.sqrt(sizeSquared());
 	}
-	
+
 	public Pos scale(double s) {
-	  return new Pos(x * s, y * s);
+		return new Pos(x * s, y * s);
 	}
-	
+
 	public Pos normalise() throws NullVectorException {
-	  double size = size();
-	  if (size == 0) {
-	    throw new NullVectorException();
-	  } else {
-	    return new Pos(x / size, y / size);
-	  }
+		double size = size();
+		if (size == 0) {
+			throw new NullVectorException();
+		} else {
+			return new Pos(x / size, y / size);
+		}
 	}
-	
+
 	public Pos perpendicular() {
 		return new Pos(y, -x);
+	}
+
+	public Pos transform(AffineTransform xf) {
+		Point2D p2 = toPoint2D();
+		xf.transform(p2, p2);
+		return new Pos(p2);
+	}
+
+	public Pos inverseTransform(AffineTransform xf) {
+		try {
+			Point2D p2 = toPoint2D();
+			xf.inverseTransform(p2, p2);
+			return new Pos(p2);
+		} catch (NoninvertibleTransformException e) {
+			throw new IllegalStateException(e);
+		}
 	}
 
 	public String toString() {
