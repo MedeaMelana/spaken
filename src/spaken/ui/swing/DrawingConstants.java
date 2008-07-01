91a82657db1604febdd7f4f1481985ef5e3166df
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-07-01 19:00:02 +0000

    Duidelijker assenstelsel.

--- src/spaken/ui/swing/DrawingConstants.java
+++ src/spaken/ui/swing/DrawingConstants.java
@@ -9,6 +9,7 @@ public class DrawingConstants {
 	public static final Color OUTLINE = new Color(0xb0b0b0);
 	public static final Color HIGHLIGHT = new Color(0xff4040);
 	public static final Color CONTROLLABLE = new Color(0x4040ff);
+	public static final Color AXIS = OUTLINE;
 
 	public static final double POINT_SIZE = 5;
 	public static final double POINT_SELECT_SIZE = 10;
