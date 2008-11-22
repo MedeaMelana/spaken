7bce6334830de7e303a796198a285613267bbc73
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2008-11-22 21:05:49 +0000

    opslaan en laden op een naieve manier gefixt. doet nu een tikje raar, maar niet raarder dan eerst :)

--- branches/stateless-elements/src/spaken/ui/swing/SpakenPanel.java
+++ branches/stateless-elements/src/spaken/ui/swing/SpakenPanel.java
@@ -118,8 +118,8 @@ public class SpakenPanel extends JPanel {
 
 	private JMenu createFileMenu() {
 		JMenu file = new JMenu("File");
-		//file.add(new HackyLoadAction(canvas));
-		//file.add(new HackySaveAction(canvas));
+		file.add(new HackyLoadAction(canvas));
+		file.add(new HackySaveAction(canvas));
 		file.add(new ExitAction());
 		return file;
 	}
