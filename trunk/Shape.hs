9da2c15cc58607820e04a14519226d8ba3bb27ce
jeroenleeuwestein <jeroenleeuwestein@65f46fdd-1e50-0410-9a28-53be5b4a9377>
2009-12-27 16:40:48 +0000

    toShapeHist

--- trunk/Shape.hs
+++ trunk/Shape.hs
@@ -3,17 +3,25 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Shape (
-  Shape(..), PointType(..),
+  Shape(..), PointType(..), SomeShape(..),
 
-  toShape,
+  ShapeHist(..),
+
+  toShape, toShapeHist,
   
-  posOf, radius, center
+  posOf, radius, center,
+
+  someShape, typedPoint, typedLine, typedCircle, typedTwoPoints
 ) where
 
 import MonadSpaken
 import Vector
 
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Control.Monad.Identity
+-- import Data.Type.Equality  -- from package type-equality
+
 
 -- Data type for shapes with calculated coördinates.
 
@@ -23,8 +31,24 @@ data Shape i where
   Circle    :: Pos         -> Double      -> Shape Circle
   TwoPoints :: Shape Point -> Shape Point -> Shape Points
 
+{-
+instance EqT Shape where
+  eqT (Point t1 p1)   (Point t2 p2)  | t1 == t2 && p1 == p2
+    = Just Refl
+  eqT (Line  p1a p1b) (Line p2a p2b) | p1a == p2a && p1b == p2b
+    = Just Refl
+  eqT (Circle p1 r1)  (Circle p2 r2) | p1 == p2 && r1 == r2
+    = Just Refl
+  eqT (TwoPoints s1a s1b) (TwoPoints s2a s2b)
+    = case (eqT s1a s2a, eqT s1b s2b) of
+      (Just Refl, Just Refl) -> Just Refl
+      _                      -> Nothing
+  eqT _                   _
+    = Nothing
+-}
+
 data PointType = Derived | Postulated
-  deriving (Eq, Show, Read)
+  deriving (Eq, Ord, Show, Read)
 
 posOf :: Shape Point -> Pos
 posOf (Point _ p) = p
@@ -35,7 +59,58 @@ radius (Circle _ r) = r
 center :: Shape Circle -> Pos
 center (Circle p _) = p
 
--- Use toShape on a generic MonadSpaken-thingy to get the result as a Shape.
+
+-- A shape, together with its dependencies (transitive). This will be useful when drawing MonadSpaken expressions.
+
+data ShapeHist i = ShapeHist
+  { shape   :: Shape i       -- The Shape itself.
+  , history :: Set SomeShape -- All Shapes that where used for calculating this Shape.
+  } deriving Show
+
+newtype ToShapeHist a = ToShapeHist (Identity a)
+  deriving (Monad)
+
+toShapeHist :: ToShapeHist a -> a
+toShapeHist (ToShapeHist m) = runIdentity m
+
+-- Lift a Shape to a ShapeHist with empty history.
+noHist :: Shape i -> ShapeHist i
+noHist = flip ShapeHist Set.empty
+
+-- Merge two ShapeHists with a new Shape.
+mergeHist :: Shape i -> ShapeHist a -> ShapeHist b -> ShapeHist i
+mergeHist sn (ShapeHist s1 h1) (ShapeHist s2 h2) = ShapeHist sn hn
+  where hn = Set.insert (someShape s1) $ Set.insert (someShape s2) $ Set.union h1 h2
+
+
+instance MonadSpaken ToShapeHist ShapeHist where
+  point p = return $ ShapeHist (Point Postulated p) Set.empty
+
+  line p1 p2 = return $ mergeHist (Line (posOf $ shape p1) (posOf $ shape p2)) p1 p2
+
+  circle p1 p2 = return $ mergeHist (Circle p1' (distance p1' p2')) p1 p2
+    where p1'  = posOf $ shape p1
+          p2'  = posOf $ shape p2
+
+  intersectCC c1 c2 = return $ mergeHist (TwoPoints (Point Derived $ i (-1)) (Point Derived $ i 1)) c1 c2
+    where i m  = (m * yt) *^ (lens + (normalized $ cross2 dist))
+          lens = p1 + xt *^ (normalized dist)
+          dist = p2 - p1
+          p1   = center $ shape c1
+          p2   = center $ shape c2
+          r1   = radius $ shape c1
+          r1s  = r1 * r1
+          r2   = radius $ shape c2
+          r2s  = r2 * r2
+          d    = magnitude dist
+          xt   = (d * d - r2s + r1s) / (2 * d)
+          yt   = sqrt ((-d + r2 - r1) * (-d -r2 + r1) * (-d + r2 + r1) * (d + r2 + r1)) / (2 * d)
+
+  bothPoints (ShapeHist s h) = case s of
+    (TwoPoints p1 p2) -> return (ShapeHist p1 h, ShapeHist p2 h)
+
+
+-- Turn a MonadSpaken expression into a Shape, without the history stuff.
 
 toShape :: ToShape i -> i
 toShape (ToShape m) = runIdentity m
@@ -44,42 +119,35 @@ newtype ToShape a = ToShape (Identity a)
   deriving (Monad)
 
 instance MonadSpaken ToShape Shape where
-  point        = return . Point Postulated
-  line   p1 p2 = return $ Line (posOf p1) (posOf p2)
-  circle p1 p2 = return $ Circle p1' (distance p1' p2')
-    where p1'  = posOf p1
-          p2'  = posOf p2
-  intersectCC c1 c2 = return $ TwoPoints (Point Derived $ i (-1)) (Point Derived $ i 1)
-    where i m  = (m * yt) *^ (lens + (normalized $ cross2 dist))
-          lens = p1 + xt *^ (normalized dist)
-          dist = p2 - p1
-          p1   = center c1
-          p2   = center c2
-          r1   = radius c1
-          r1s  = r1 * r1
-          r2   = radius c2
-          r2s  = r2 * r2
-          d    = magnitude dist
-          xt   = (d * d - r2s + r1s) / (2 * d)
-          yt   = sqrt ((-d + r2 - r1) * (-d -r2 + r1) * (-d + r2 + r1) * (d + r2 + r1)) / (2 * d)
+  point       = return . shape . toShapeHist . point
+  line        = stripHist line
+  circle      = stripHist circle
+  intersectCC = stripHist intersectCC
   bothPoints (TwoPoints p1 p2) = return (p1, p2)
 
--- Shape data type stripped of type information.
--- Primarily for 'easy' deriving of Show. Probably overkill.
+stripHist f p1 p2 = return . shape . toShapeHist $ f (noHist p1) (noHist p2)
 
-data StrippedShape
+
+-- Shape data type stripped of type information.
+
+data SomeShape
   = APoint PointType Pos
   | ALine  Pos Pos
   | ACircle Pos Double
-  | ATwoPoints StrippedShape StrippedShape
-  deriving (Eq, Show, Read)
+  | ATwoPoints SomeShape SomeShape
+  deriving (Eq, Ord, Show, Read)
 
-strip :: Shape i -> StrippedShape
-strip (Point t p)       = APoint t p
-strip (Line p1 p2)      = ALine p1 p2
-strip (Circle p r)      = ACircle p r
-strip (TwoPoints p1 p2) = ATwoPoints (strip p1) (strip p2)
+someShape :: Shape i -> SomeShape
+someShape (Point t p)       = APoint t p
+someShape (Line p1 p2)      = ALine p1 p2
+someShape (Circle p r)      = ACircle p r
+someShape (TwoPoints p1 p2) = ATwoPoints (someShape p1) (someShape p2)
+
+typedPoint (APoint t p) = Point t p
+typedLine (ALine p1 p2) = Line p1 p2
+typedCircle (ACircle p r) = Circle p r
+typedTwoPoints (ATwoPoints p1 p2) = TwoPoints (typedPoint p1) (typedPoint p2)
 
 instance Show (Shape i) where
-  show = show . strip
+  show = filter (/= 'A') . show . someShape  -- evil hack!
 
